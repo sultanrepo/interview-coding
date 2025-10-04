27f116d2eaceea02b9bf702ac12e1d2784c79289
Sultan Ashraf <sultanashraf.repo@gmail.com>
2025-10-04 09:54:11 +0530

    Learning

--- interviewQuestions/debouncing/script.js
+++ interviewQuestions/debouncing/script.js
@@ -1,17 +1,35 @@
+//Debouncing
 let count = 0;
-function fetchData(event) {
-    console.log("Fetching Data....", count++);
+function fetchData() {
+    console.log("Data Fetching....", count++);
 }
 
 function debounce(callBack, delay) {
-    let timerId;
-    return function(...args){
-        clearTimeout(timerId);
-        timerId = setTimeout(() => {
+    let timerID;
+    return function(...args) {
+        if(timerID){
+            clearTimeout(timerID);
+        }
+        timerID = setTimeout(() => {
             callBack();
-        }, delay);
+        }, delay)
     }
 }
 
-betterFunction = debounce(fetchData, 1000);
+betterFunction = debounce(fetchData, 500);
 
+//Throttling
+let count_ = 0;
+function fetchUser() {
+    console.log("Fetching User...", count_++);
+}
+
+function throttling(callBack, delay) {
+    return function(...args) {
+        setInterval(() => {
+            callBack();
+        }, delay)
+    }
+}
+
+throttlingFunction = throttling(fetchUser, 1000);
