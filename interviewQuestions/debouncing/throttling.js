6eff52415438bf894c425380ad2149104f2ef7b2
Sultan Ashraf <sultanashraf.repo@gmail.com>
2025-10-08 00:31:25 +0530

    Throttle In JavaScript

--- interviewQuestions/debouncing/throttling.js
+++ interviewQuestions/debouncing/throttling.js
@@ -1,7 +1,21 @@
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
+
 let count = 0;
-function handleScroll() {
-    console.log("Scrolling....", count++);
+function scrollFunction() {
+    console.log('API Call....!', count++);
 }
 
-container.addEventListener("scroll", handleScroll);
+function throttle(callBackFunc, delay) {
+    let lastCall = 0;
+    return function (...args) {
+        let now = Date.now();
+        if (now - lastCall >= delay) {
+            callBackFunc();
+            lastCall = now;
+        }
+    }
+}
+
+const betterFunction = throttle(scrollFunction, 2000);
+
+container.addEventListener('scroll', betterFunction);
